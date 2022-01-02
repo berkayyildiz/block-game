49dbeb19fa730b3f732c28dc9a8d33738b1c9a51
berkayyildiz <berkayyildizgm@gmail.com>
2022-01-02 22:19:50 +0300

    block creation, zooming and moving camera

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,72 @@
-import { Actor, CollisionType, Color, Engine, vec } from "excalibur";
+import { Actor, CollisionType, Color, Engine, Input, Vector } from "excalibur";
 
 let draggingActor: Actor;
 
 const game = new Engine({
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 });
 
 // Start the engine to begin the game.
 game.start();
 
-const paddle = new Actor({
-  x: 150,
-  y: game.drawHeight - 40,
-  width: 100,
-  height: 40,
-  color: Color.Gray,
-});
+const boxes: Actor[] = [];
 
-paddle.body.collisionType = CollisionType.Fixed;
+function addBox() {
+  const box = new Actor({
+    x: game.drawWidth / 2 - 50,
+    y: game.drawHeight / 2 - 20,
+    width: 100,
+    height: 40,
+    color: Color.Gray,
+  });
+  box.body.collisionType = CollisionType.Fixed;
+  box.enableCapturePointer = true;
 
-game.add(paddle);
+  box.on("pointerdown", (event) => {
+    draggingActor = box;
+  });
 
-paddle.enableCapturePointer = true;
+  box.on("pointerup", (event) => {
+    draggingActor = null;
+  });
+  boxes.push(box);
+  game.add(box);
+}
 
-paddle.on("pointerdown", (event) => {
-  draggingActor = paddle;
-});
+let lastPointerPos: Vector;
 
-paddle.on("pointerup", (event) => {
-  draggingActor = null;
+game.input.pointers.primary.on("down", (event) => {
+  lastPointerPos = game.input.pointers.primary.lastWorldPos;
 });
 
 game.input.pointers.primary.on("move", (event) => {
-  if (draggingActor) {
-    draggingActor.pos.x = event.pointer.lastWorldPos.x;
-    draggingActor.pos.y = event.pointer.lastWorldPos.y;
+  if (event.pointer.isDragging) {
+    if (draggingActor) {
+      draggingActor.pos.x = event.pointer.lastWorldPos.x;
+      draggingActor.pos.y = event.pointer.lastWorldPos.y;
+    } else {
+      const xShift =
+        game.input.pointers.primary.lastWorldPos.x - lastPointerPos.x;
+      const yShift =
+        game.input.pointers.primary.lastWorldPos.y - lastPointerPos.y;
+
+      const factor = -0.75; //Higher factors create wiggle on actors
+      game.currentScene.camera.x += xShift * factor;
+      game.currentScene.camera.y += yShift * factor;
+
+      lastPointerPos = game.input.pointers.primary.lastWorldPos;
+    }
+  }
+});
+
+game.input.pointers.primary.on("wheel", (event) => {
+  const factor = 0.001;
+  game.currentScene.camera.zoom -= event.deltaY * factor;
+});
+
+game.input.keyboard.on("press", (event) => {
+  if (event.key == Input.Keys.B) {
+    addBox();
   }
 });
